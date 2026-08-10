415bea73b38ca4fa414b2028d5f1862d259012b3
Santi <santi@pulpsense.com>
2026-08-10 16:32:17 +0200

    Remove unused funnel scaffolding

--- apps/funnels/src/components/funnel/index.ts
+++
@@ -1,9 +0,0 @@
-export { FunnelChecklist } from './FunnelChecklist';
-export { FunnelCta } from './FunnelCta';
-export { FunnelLegalFooter } from './FunnelLegalFooter';
-export type { FunnelLegalLink } from './FunnelLegalFooter';
-export { FunnelLogoMarquee } from './FunnelLogoMarquee';
-export type { FunnelLogo } from './FunnelLogoMarquee';
-export { FunnelPage } from './FunnelPage';
-export { FunnelSection } from './FunnelSection';
-export { FunnelVideoGrid } from './FunnelVideoGrid';
